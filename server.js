345a1861f62ac0d712bd75e6c027a511f608c827
popov7 <deniolp@gmail.com>
2019-02-17 15:31:07 +0400

    Making simple answer to POST request that sending by form

--- server.js
+++ server.js
@@ -27,9 +27,21 @@ server.get('/', (req, res) => {
 });
 
 server.post('/', (req, res) => {
-  console.log(req.body);
-
-  res.send('Приветикус!');
+  res.send(`
+  <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>Предложения</title>
+    </head>
+    <body>
+      <p>Привет, ${req.body.username}</p>
+      </form>
+    </body>
+  </html>
+  `);
 });
 
 server.listen(3000, 'localhost', () => console.log('Сервер запущен!'));
