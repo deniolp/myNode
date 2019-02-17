4402d45e0e0559d8f776eed838a4cfa70701d409
popov7 <deniolp@gmail.com>
2019-02-17 14:28:29 +0400

    Start to customize express

--- server.js
+++ server.js
@@ -2,4 +2,11 @@ const express = require('express');
 
 const server = express();
 
+server.get('/', (req, res) => {
+  console.log(req.hostname);
+  console.log(req.url);
+
+  res.sendFile('./index.html', { root: __dirname });
+});
+
 server.listen(3000, 'localhost', () => console.log('Сервер запущен!'));
