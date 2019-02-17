4e9bc69baac81bafbd8df3014f9d7b1e5dcc7bd1
popov7 <deniolp@gmail.com>
2019-02-17 14:17:43 +0400

    Installing express framework for working with node.js server

--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const http = require('http');
+const express = require('express');
 
-http.createServer((req, res) => {
-  console.log(req.method);
-  console.log(req.url);
-  console.log(req.headers);
+const server = express();
 
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
-  });
-
-  res.end(`
-    <h1>Приветикус!</h1>
-  `);
-}).listen(3000, '127.0.0.1', () => console.log('Сервер запущен!'));
+server.listen(3000, 'localhost', () => console.log('Сервер запущен!'));
