18002538b4281020ed9ab2490933ff51a4487f11
popov7 <deniolp@gmail.com>
2019-02-17 16:11:47 +0400

    Making redirect from post to get /

--- server.js
+++ server.js
@@ -41,21 +41,7 @@ server.get('/', (req, res) => {
 server.post('/', (req, res) => {
   res.cookie('username', req.body.username);
 
-  res.send(`
-  <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Предложения</title>
-    </head>
-    <body>
-      <p>Привет, ${req.body.username}</p>
-      </form>
-    </body>
-  </html>
-  `);
+  res.redirect('/');
 });
 
 server.listen(3000, 'localhost', () => console.log('Сервер запущен!'));
