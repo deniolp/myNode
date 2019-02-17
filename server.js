c28722bd38f1a8b923007c4f11cb2d37e3244ccb
popov7 <deniolp@gmail.com>
2019-02-17 13:45:41 +0400

    Refactoring this plain node.js server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 const http = require('http');
 
-const server = http.createServer();
-
-server.on('request', (req, res) => {
+http.createServer((req, res) => {
   console.log(req.method);
   console.log(req.url);
   console.log(req.headers);
 
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
 
-  res.end('Приветикус!');
-});
-
-server.listen(3000, '127.0.0.1', () => console.log('Сервер запущен!'));
+  res.end(`
+    <h1>Приветикус!</h1>
+  `);
+}).listen(3000, '127.0.0.1', () => console.log('Сервер запущен!'));
