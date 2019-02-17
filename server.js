8b6595f6a47d83606fda9e7b98e5301ad7f70b51
popov7 <deniolp@gmail.com>
2019-02-17 13:13:58 +0400

    Looking at req object in console

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const http = require('http');
 
 const server = http.createServer();
 
-server.on('request', () => {
-  console.log('Request');
+server.on('request', (req, res) => {
+  console.log(req.method);
+  console.log(req.url);
+  console.log(req.headers);
 });
 
 server.listen(3000, '127.0.0.1', () => console.log('Сервер запущен!'));
