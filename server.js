3f4b4b35efda64fc1d27608ab8b4f14626345b89
popov7 <deniolp@gmail.com>
2019-02-17 15:05:57 +0400

    Making answer from server by res.send with html with form

--- server.js
+++ server.js
@@ -3,10 +3,25 @@ const express = require('express');
 const server = express();
 
 server.get('/', (req, res) => {
-  console.log(req.hostname);
-  console.log(req.url);
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Предложения</title>
+  </head>
+  <body>
+    <h1>Вход</h1>
 
-  res.sendFile('./index.html', { root: __dirname });
+    <form>
+      <input type="text" name="username">
+      <button type="submit">Войти</button>
+    </form>
+  </body>
+  </html>
+  `);
 });
 
 server.listen(3000, 'localhost', () => console.log('Сервер запущен!'));
